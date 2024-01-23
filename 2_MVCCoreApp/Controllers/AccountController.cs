09cec22b102c629ed049db1ccca36b2d7f009d03
vikul rathod <vikul.rathod.net@gmail.com>
2024-01-23 08:51:43 +0530

    mvc core session 2

---
+++ 2_MVCCoreApp/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using _2_MVCCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2_MVCCoreApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        // [NonAction]
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            if(email.Equals("admin") && password.Equals("admin"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("account/welcome/{firstName}/{lastName}")]
+        public ViewResult Welcome(string firstName, string lastName)
+        {
+            ViewBag.FirstName = firstName;
+            ViewBag.LastName = lastName;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(LoginViewModel model)
+        {
+            if(model.Email.Equals("admin@gmail.com") &&
+                model.Password.Equals("admin"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ 2_MVCCoreApp/Models/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace _2_MVCCoreApp.Models
+{
+    public class LoginViewModel
+    {
+        [DisplayName("Registered Email")]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
